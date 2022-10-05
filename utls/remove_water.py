8dd59f4c9a108941f9a910470bc18a6b1ae594c3
Ghaith Mqawass <71670961+ghaith-mq@users.noreply.github.com>
2022-10-05 19:01:03 +0200

    Update remove_water.py

--- utls/remove_water.py
+++ utls/remove_water.py
@@ -1,13 +1,10 @@
 import os
-dirs=os.listdir()
-dirs.sort()
-
-for dir_ in dirs:
-	files=os.listdir(dir_)
-	for file_ in files:
-		if (file_[-5:]=="n.pdb"):
-			command= "grep -vwE" + " \" HOH \" " + dir_ + "/" + file_ + " > " + dir_ + "/" + dir_ + "_nowater.pdb" 
-			os.system(command)
+dir_= "path to the directory where PDB files are located"
+files=os.listdir(dir_)
+for file_ in files:
+	if (file_[-5:]=="n.pdb"):
+		command= "grep -vwE" + " \" HOH \" " + dir_ + "/" + file_ + " > " + dir_ + "/" + dir_ + "_nowater.pdb" 
+		os.system(command)
 
 
 
